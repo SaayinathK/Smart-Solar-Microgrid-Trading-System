3d3224b19ec9ffb0f117bc256c537eec0dcdf017
SaayinathK <k.saayinath@gmail.com>
2026-09-23 17:14:58 +0530

    feat(api): update mongo context and db seeder for m1

--- Backend/SmartMicrogrid.API/Data/MongoCollections.cs
+++ Backend/SmartMicrogrid.API/Data/MongoCollections.cs
@@ -3,8 +3,8 @@ namespace SmartMicrogrid.API.Data
     public static class MongoCollections
     {
         public const string Users = "users";
-        public const string MicrogridNodes = "microgrid_nodes";
-        public const string CapacitySlots = "capacity_slots";
+        public const string Microgrids = "microgrids";
+        public const string EnergySlots = "energySlots";
         public const string Reservations = "reservations";
         public const string Transactions = "transactions";
     }
