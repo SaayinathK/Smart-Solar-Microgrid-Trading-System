c64bd50de3868e3149b868feb3acb2b33edcaf3e
Jathusan-Jesuthasan <jesuthasanjathusan@gmail.com>
2026-09-24 09:52:34 +0530

    feat(transaction): implement GenerateQrResponse data class for QR code generation

--- Android/SmartMicrogrid.Android/app/src/main/java/com/smartmicrogrid/models/GenerateQrResponse.kt
+++ Android/SmartMicrogrid.Android/app/src/main/java/com/smartmicrogrid/models/GenerateQrResponse.kt
@@ -1,4 +1,20 @@
 package com.smartmicrogrid.models
 
-class GenerateQrResponse {
-}
+import com.google.gson.annotations.SerializedName
+
+data class GenerateQrResponse(
+    @SerializedName("transactionId")
+    val transactionId: String = "",
+
+    @SerializedName("reservationId")
+    val reservationId: String = "",
+
+    @SerializedName("transactionCode")
+    val transactionCode: String = "",
+
+    @SerializedName("qrCodeData")
+    val qrCodeData: String = "",
+
+    @SerializedName("status")
+    val status: String = ""
+)
